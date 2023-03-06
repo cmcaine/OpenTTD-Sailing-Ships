7d2c6e3b8ad4ee2b390ff79cc53205c075ebd53d
Colin Caine <cmcaine@gmail.com>
2023-03-06 01:26:33 +0000

    Add comments to compile script

--- compile.bat
+++ compile.bat
@@ -1,1 +1,3 @@
-c:\dev\nml\nmlc -c --grf ss07.grf sailingships_0.7.nml
+: # We use --quiet because otherwise nmlc complains about the sprites not being marked as ANIM.
+: # These warnings can be ignored because nmlc will generate animated sprites regardless of the ANIM/NOANIM flag.
+nmlc --quiet -c --grf ss.grf sailingships_0.7.nml
